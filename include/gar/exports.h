526aa6c692d72f5f2dc54d0b1a112ab3a327e2d9
Rokr0k <rokr0k@rokr0k.xyz>
2026-03-25 14:37:01 +0900

    add option to switch between dynamic and static library

--- include/gar/exports.h
+++ include/gar/exports.h
@@ -2,10 +2,14 @@
 #define GAR_EXPORTS_H
 
 #ifdef _WIN32
+#ifndef GAR_STATIC
 #ifdef gar_EXPORTS
 #define GAR_API __declspec(dllexport)
 #else
 #define GAR_API __declspec(dllimport)
+#endif
+#else
+#define GAR_API
 #endif
 #else
 #define GAR_API
